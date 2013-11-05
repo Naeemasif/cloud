a336bdeedae5f53314c0754d6d7b37ba01d3049a
naeem asif <naeemgoraya@yahoo.com>
2013-11-05 13:36:17 +0500

    sms module added

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -18,23 +18,17 @@ class ResumesController < ApplicationController
      @user = current_user
      @resume = Resume.new(params[:resume])
      @resume.user_id = @user.id
-    #  if current_user.space.to_i == 0
-     #   flash[:file_status] = "File Cannot be Uploaded"
-      #  redirect_to :action =>"index"
-      #end
-      #if (current_user.resumes.last.file_size/(1024*1024)) .to_i > current_user.space.to_i
-       # flash[:file_status] = "File Cannot be Uploaded size greater than expected"
-        #redirect_to :action =>"index"
-      #end
+      if current_user.space.to_i == 0
+          render "sms"
 
-    if @resume.save
+    elsif @resume.save
       a=current_user.space.to_i - (current_user.resumes.last.file_size/(1024*1024)).to_i
       b=(current_user.resumes.last.file_size/(1024*1024)).to_i + current_user.allocated_space.to_i
       current_user.update_attributes(:space => a.to_i.to_s,:allocated_space=>b.to_i.to_s)
       redirect_to resumes_path, notice: "The resume #{@resume.name} has been uploaded."
     else
       render "new"
-    end
+  end
   end
 
   def destroy
